5e710b32c4b20ec081786b0ad9fa52ebec5cbc5c
Seungwon Park 박승원 <yyyyy@snu.ac.kr>
2019-10-25 13:41:32 +0900

    Resolves #8 (#12)
    * resolve clicking sound artifact (#8)
    
    * fix inference.py to generate 16bit PCM (int16)

--- inference.py
+++ inference.py
@@ -2,11 +2,13 @@ import os
 import glob
 import tqdm
 import torch
-import librosa
 import argparse
+from scipy.io.wavfile import write
 
 from model.generator import Generator
 from utils.hparams import HParam, load_hparam_str
+
+MAX_WAV_VALUE = 32768.0
 
 
 def main(args):
@@ -27,12 +29,21 @@ def main(args):
                 mel = mel.unsqueeze(0)
             mel = mel.cuda()
 
+            # pad input mel with zeros to cut artifact
+            # see https://github.com/seungwonpark/melgan/issues/8
+            zero = torch.full((1, hp.audio.n_mel_channels, 10), -11.5129).cuda()
+            mel = torch.cat((mel, zero), axis=2)
+
             audio = model(mel)
-            audio = audio.squeeze()
+            audio = audio.squeeze() # collapse all dimension except time axis
+            audio = audio[:-(hp.audio.hop_length*10)]
+            audio = MAX_WAV_VALUE * audio
+            audio = audio.clamp(min=-MAX_WAV_VALUE, max=MAX_WAV_VALUE)
+            audio = audio.short()
             audio = audio.cpu().detach().numpy()
 
             out_path = melpath.replace('.mel', '_reconstructed_epoch%04d.wav' % checkpoint['epoch'])
-            librosa.output.write_wav(out_path, audio, sr=hp.audio.sampling_rate)
+            write(out_path, hp.audio.sampling_rate, audio)
 
 
 if __name__ == '__main__':
